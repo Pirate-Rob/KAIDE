98c77043397d312b11a0ff6372f135854e04d001
Pirate-Rob <bobwilliampss@gmail.com>
2023-04-20 18:23:29 +0200

    Trying to get CFG tab to be designable

--- CFGTab.cs
+++ CFGTab.cs
@@ -12,12 +12,14 @@ namespace KAGIDE
 {
     internal class CFGTab : TabPage
     {
+        private Label label1;
+
         public CFGTab(string path)
         {
             this.Text = Path.GetFileName(path);
             this.Tag = path;
 
-
+            /*
             ToolStrip toolStrip = CreateTabToolStrip(
             saveButtonAction: () =>
             {
@@ -38,6 +40,9 @@ namespace KAGIDE
 
             panel.Controls.Add(toolStrip);
             this.Controls.Add(panel);
+            */
+
+            InitializeComponent();
 
         }
 
@@ -58,6 +63,29 @@ namespace KAGIDE
             toolStrip.Items.Add(closeButton);
 
             return toolStrip;
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
